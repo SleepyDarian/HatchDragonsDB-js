71993a33ccb6ccae7446da173aab9e4388b3b76d
Zechary Chua <zechary28@gmail.com>
2026-05-05 01:15:22 +0800

    Barely presentable

---
+++ crop.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+img = Image.open("app/static/dragons/photo_2026-05-05_00-54-47.jpg")
+w, h = img.size
+size = 128
+x_offset = 48
+y_offset = 18
+left = 183
+top  = 2144
+print(w, h)
+print(size)
+print(left)
+print(top)
+cropped = img.crop((left, top, left + size, top + size))
+cropped.save("app/static/dragons/Alp Angel.jpg")
